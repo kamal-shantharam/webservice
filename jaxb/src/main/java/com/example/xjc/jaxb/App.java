8acb34ca9bfe4286297ddc012a5535c9b0e08a20
Kamal Shantharam <kamalshantharam@gmail.com>
2019-09-16 07:26:46 -0400

    jaxb files

---
+++ jaxb/src/main/java/com/example/xjc/jaxb/App.java
@@ -0,0 +1,13 @@
+package com.example.xjc.jaxb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
