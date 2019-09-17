b2dc1b98a83627043feff2afac89f660cfe0d6be
Kamal Shantharam <kamalshantharam@gmail.com>
2019-09-16 21:40:09 -0400

    jaxb demo

--- jaxb/src/main/java/com/example/xjc/jaxb/App.java
+++ jaxb/src/main/java/com/example/xjc/jaxb/App.java
@@ -1,5 +1,15 @@
 package com.example.xjc.jaxb;
 
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import com.example.patient.Patient;
+
 /**
  * Hello world!
  *
@@ -8,6 +18,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	try {
+			JAXBContext context = JAXBContext.newInstance(Patient.class);
+			Marshaller marshal = context.createMarshaller();
+			Patient patient1 = new Patient();
+			patient1.setAge(30);
+			patient1.setName("Raman");
+			StringWriter stringWriter = new StringWriter();
+			marshal.marshal(patient1, stringWriter);
+			System.out.println(stringWriter.toString());
+			
+			Unmarshaller unmarshal = context.createUnmarshaller();
+			Patient patient2 = (Patient) unmarshal.unmarshal(new StringReader(stringWriter.toString()));
+			System.out.println(patient2.getName() + "-" + patient2.getAge());
+			
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
     }
 }
