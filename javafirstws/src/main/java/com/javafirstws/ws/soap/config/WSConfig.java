a1692abac1cc5243bc575b318d6c7246a65890e9
Kamal Shantharam <kamalshantharam@gmail.com>
2019-10-04 07:25:07 -0400

    user name token security feature added

--- javafirstws/src/main/java/com/javafirstws/ws/soap/config/WSConfig.java
+++ javafirstws/src/main/java/com/javafirstws/ws/soap/config/WSConfig.java
@@ -1,9 +1,14 @@
 package com.javafirstws.ws.soap.config;
 
+import java.util.HashMap;
+
 import javax.xml.ws.Endpoint;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.wss4j.common.ConfigurationConstants;
+import org.apache.wss4j.dom.WSConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +23,16 @@ public class WSConfig {
 	
 	@Bean
 	public Endpoint endpoint() {
-		Endpoint endpoint = new EndpointImpl(bus, new PaymentProcessorImpl());
+		EndpointImpl endpoint = new EndpointImpl(bus, new PaymentProcessorImpl());
 		endpoint.publish("/javafirstws");
+		
+		HashMap<String, Object> props = new HashMap<>();
+		props.put(ConfigurationConstants.ACTION, ConfigurationConstants.USERNAME_TOKEN);
+		props.put(ConfigurationConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+		props.put(ConfigurationConstants.PW_CALLBACK_CLASS, UTPasswordCallback.class.getName());
+		WSS4JInInterceptor wssIn = new WSS4JInInterceptor(props);
+		endpoint.getInInterceptors().add(wssIn);
+		
 		return endpoint;
 	}
 
