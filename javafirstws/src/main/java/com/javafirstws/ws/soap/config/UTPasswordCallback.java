0cdd8a205955adb984c00a38b5b00f8d6559002a
Kamal Shantharam <kamalshantharam@gmail.com>
2019-10-24 21:37:39 -0400

    exception handling

---
+++ javafirstws/src/main/java/com/javafirstws/ws/soap/config/UTPasswordCallback.java
@@ -0,0 +1,33 @@
+package com.javafirstws.ws.soap.config;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+public class UTPasswordCallback implements CallbackHandler {
+
+	Map<String, String> passwords = new HashMap<>();
+	
+	public UTPasswordCallback() {
+		passwords.put("cxf", "cxf");
+	}
+	
+	@Override
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+
+		for(Callback callback : callbacks) {
+			WSPasswordCallback wscallback = (WSPasswordCallback) callback;
+			String password = passwords.get(wscallback.getIdentifier());
+			if (password != null && password.equals("cxf")) {
+				wscallback.setPassword(password);
+			}
+		}
+	}
+
+}
