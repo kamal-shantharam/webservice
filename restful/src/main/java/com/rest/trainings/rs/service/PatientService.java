79529a59bb33e400ba1253b1b0c1c33f518a0f8e
Kamal Shantharam <kamalshantharam@gmail.com>
2019-11-09 09:31:03 -0500

    JSON support

--- restful/src/main/java/com/rest/trainings/rs/service/PatientService.java
+++ restful/src/main/java/com/rest/trainings/rs/service/PatientService.java
@@ -2,36 +2,43 @@ package com.rest.trainings.rs.service;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 import com.rest.trainings.rs.model.Patient;
 
+@Consumes("application/xml,application/json")
+@Produces("application/xml,application/json")
+//@Consumes(MediaType.APPLICATION_JSON)
+//@Produces(MediaType.APPLICATION_JSON)
 @Path("/patientservice")
 public interface PatientService {
 	
 	@Path("/patients")
 	@GET
-	List<Patient> getPatients();
+	public List<Patient> getPatients();
 	
 	@Path("/patients/{id}")
 	@GET
-	Patient getPatient(@PathParam("id") Long patientId);
+	public Patient getPatient(@PathParam("id") Long patientId);
 	
 	@Path("/patients")
 	@POST
-	Response createPatient(Patient patient);
+	public Response createPatient(Patient patient);
 	
 	@Path("/patients")
 	@PUT
-	Response updatePatient(Patient patient);
+	public Response updatePatient(Patient patient);
 	
 	@Path("/patients/{id}")
 	@DELETE
-	Response deletePatient(@PathParam("id") Long patientId);
+	public Response deletePatient(@PathParam("id") Long patientId);
 }
